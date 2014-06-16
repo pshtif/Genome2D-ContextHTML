bd633c1a1e162b6078b2874960a6e3c4c17750ec
Peter "sHTiF" Stefcek <pshtif@gmail.com>
2014-06-16 16:52:25 +0200

    Documentation & Cleanup

--- src/com/genome2d/context/GContextCamera.hx
+++ src/com/genome2d/context/GContextCamera.hx
@@ -14,29 +14,29 @@ class GContextCamera {
     public var x:Float = 0;
     public var y:Float = 0;
 
-    /*
-	 * 	Camera mask used against node camera group a node is rendered through this camera if camera.mask and nodecameraGroup != 0
-	 */
+    /**
+	    Camera mask used against node camera group a node is rendered through this camera if camera.mask and nodecameraGroup != 0
+	**/
     public var mask:Int = 0xFFFFFF;
 
-    /*
-	 * 	Viewport x offset, this value should be always within 0 and 1 its based on context main viewport
-	 */
+    /**
+	    Viewport x offset, this value should be always within 0 and 1 its based on context main viewport
+	**/
     public var normalizedViewX:Float = 0;
 
-    /*
-	 * 	Viewport y offset, this value should be always within 0 and 1 it based on context main viewport
-	 */
+    /**
+	    Viewport y offset, this value should be always within 0 and 1 it based on context main viewport
+	**/
     public var normalizedViewY:Float = 0;
 
-    /*
-	 * 	Viewport width, this value should be always within 0 and 1 its based on context main viewport
-	 */
+    /**
+	    Viewport width, this value should be always within 0 and 1 its based on context main viewport
+	**/
     public var normalizedViewWidth:Float = 1;
 
-    /*
-	 * 	Viewport height, this value should be always within 0 and 1 its  based on context main viewport
-	 */
+    /**
+	    Viewport height, this value should be always within 0 and 1 its  based on context main viewport
+	**/
     public var normalizedViewHeight:Float = 1;
 
     public function new() {
